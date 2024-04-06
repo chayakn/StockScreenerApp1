2c181f84529756ca070680525ebbba2f322ab1bf
swathi0710 <117807966+swathi0710@users.noreply.github.com>
2024-04-06 16:59:33 +0530

    Update St.py
    indent fix

--- St.py
+++ St.py
@@ -20,31 +20,31 @@ import google.generativeai as genai
 
 def gem_chat():
 	try:
-		gemini_api_key = os.environ['GOOGLE_GEMINI_KEY']
-		genai.configure(api_key=gemini_api_key)
-		model = genai.GenerativeModel('gemini-pro')
-
-		if "chat" not in st.session_state:
-		  st.session_state.chat = model.start_chat(history=[])
-		st.title('Gemini Pro Test')
-
-		def role_to_streamlit(role: str) -> str:
-		  if role == 'model':
-			return 'assistant'
-		  else:
-			return role
-
-		for message in st.session_state.chat.history:
-		  with st.chat_message(role_to_streamlit(message.role)):
-			st.markdown(message.parts[0].text)
-
-		if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
-		  st.chat_message("user").markdown(prompt)
-		  response = st.session_state.chat.send_message(prompt)
-		  with st.chat_message("assistant"):
-			st.markdown(response.text)
+	    gemini_api_key = os.environ['GOOGLE_GEMINI_KEY']
+	    genai.configure(api_key=gemini_api_key)
+	    model = genai.GenerativeModel('gemini-pro')
+	
+	    if "chat" not in st.session_state:
+	      st.session_state.chat = model.start_chat(history=[])
+	    st.title('Gemini Pro Test')
+	
+	    def role_to_streamlit(role: str) -> str:
+	      if role == 'model':
+	        return 'assistant'
+	      else:
+	        return role
+	
+	    for message in st.session_state.chat.history:
+	      with st.chat_message(role_to_streamlit(message.role)):
+	        st.markdown(message.parts[0].text)
+	
+	    if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
+	      st.chat_message("user").markdown(prompt)
+	      response = st.session_state.chat.send_message(prompt)
+	      with st.chat_message("assistant"):
+	        st.markdown(response.text)
 	except Exception as e:
-		st.error(f'An error occurred: {e}')
+	    st.error(f'An error occurred: {e}')
 
 
 def bidirectional_slider(label, min_value, max_value, default_value):
