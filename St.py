4bd3c4ebb7a096f862ff7403dee20d3fbfe9e01b
surbhijha17 <36736220+surbhijha17@users.noreply.github.com>
2024-04-06 13:27:29 +0530

    Update St.py
    bug fix

--- St.py
+++ St.py
@@ -190,7 +190,7 @@ def main():
         window_size = st.sidebar.slider('Moving Average Window Size', min_value=1, max_value=30, value=10)
         # Calculate moving averages
         for col in selected_data.columns[1:]:
-            selected_df[f'{col} Moving Average'] = selected_data[col].rolling(window=window_size).mean()
+            selected_data[f'{col} Moving Average'] = selected_data[col].rolling(window=window_size).mean()
         
         # Create a Plotly figure
         fig = go.Figure()
@@ -200,7 +200,7 @@ def main():
             fig.add_trace(go.Scatter(x=selected_data['Date'], y=selected_data[col], mode='lines', name=col))
         
         # Add moving average traces
-        for col in selected_df.columns[1:]:
+        for col in selected_data.columns[1:]:
             fig.add_trace(go.Scatter(x=selected_data['Date'], y=selected_data[f'{col} Moving Average'], mode='lines', name=f'{col} Moving Average', line=dict(color='red')))
         
         # Update layout
