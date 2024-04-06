12c8341106dafa4372efda013ef4cda4cda2d7e0
surbhijha17 <36736220+surbhijha17@users.noreply.github.com>
2024-04-06 17:41:18 +0530

    Update St.py
    jkhklj

--- St.py
+++ St.py
@@ -81,14 +81,14 @@ def calculate_metrics_for_all_stocks(data):
         # columns_with_bidirectional_slider = ['Volume', 'Market Capping', 'Change percentage', 'P/E', 'Dividend Yield %']
         columns_with_bidirectional_slider = ['Change percentage', 'P/E', 'Dividend Yield %']
         # Filter Rows by close column
-        selected_data = summary_data
-        for col in columns_with_bidirectional_slider:
-            try:
-                low, up = bidirectional_slider(col, min_value=selected_data[col].min(), max_value=selected_data[col].max(), default_value=(0.0, 30.0))
-                selected_data = selected_data[(selected_data[col] >= low) & (selected_data[col] <= up)]
-            except:
-                pass
-    return selected_data
+        data = summary_data
+        # for col in columns_with_bidirectional_slider:
+        #     try:
+        #         low, up = bidirectional_slider(col, min_value=selected_data[col].min(), max_value=selected_data[col].max(), default_value=(0.0, 30.0))
+        #         selected_data = selected_data[(selected_data[col] >= low) & (selected_data[col] <= up)]
+        #     except:
+        #         pass
+    return data
     
 def decompose_time_series(data):
     result = seasonal_decompose(data, model='additive', period=1)
